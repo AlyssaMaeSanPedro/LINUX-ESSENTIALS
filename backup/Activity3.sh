e92efd921ccf79e1b651626b1a96862a6d0f2228
AlyssaMaeSanPedro <163331805+AlyssaMaeSanPedro@users.noreply.github.com>
2025-04-23 04:27:18 +0000

    commit

--- backup/Activity3.sh
+++
@@ -1,7 +0,0 @@
-echo "Enter your name: ";
-read NAME;
-echo "Enter your age: ";
-read AGE;
-echo "Enter your favorite programming language: ";
-read PROGRAMMING;
-echo "Thank you $NAME, You are $AGE years old & your favorite programming language is $PROGRAMMING.";
--- backup/log_time.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "Current Date and Time: $(TZ=Asia/Manila date '+%a %b %d, %Y %H:%M:%S')" >> log.txt
